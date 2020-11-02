71c0908e8db6530bcc26e796c9782aa7b2d6e92e
Heragoga <heragogaa1@gmail.com>
2020-11-02 11:58:02 +0100

    ffff

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ const client = new Discord.Client();
 client.once('ready', () => {
 console.log('Karol lebt!')
     client.user.setActivity("with Alex!")
-    console.log("Servers:")
+//    console.log("Servers:")
 
-    client.guilds.cache.forEach((guild) => {
-        console.log(" - " + guild.name)
-
-        guild.channels.cache.forEach((channel) => {
-            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
-        })
-    });
-    let botcommands = client.channels.cache.get("763467469942947840")
+ //   client.guilds.cache.forEach((guild) => {
+//        console.log(" - " + guild.name)
+//
+//        guild.channels.cache.forEach((channel) => {
+//            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+//        })
+//    });
+//    let botcommands = client.channels.cache.get("763467469942947840")
 });
 client.on("message", msg => {
     if (msg.content === "/help"){

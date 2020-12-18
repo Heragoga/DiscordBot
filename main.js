4cb78f813cd321ed21a2384371bae9e36b0ffd55
Heragoga <heragogaa1@gmail.com>
2020-12-18 12:30:43 +0100

    w

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ client.on('message', async message => {
         const connection = await message.member.voice.channel.leave();
     }
     // Create a dispatcher
-    //oiwqjd
+    //oiwqj
 });
 function commands () {
     client.on("message", msg => {
